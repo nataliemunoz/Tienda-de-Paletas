9b26ee02c9d53f148e96935742e326fb35bebedc
Natalie Muñoz <75344403+nataliemunoz@users.noreply.github.com>
2023-04-01 14:32:23 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function compra() {
+  var unidad = prompt("Cuantas paletas desea Ordenar? ");
+}
+
+var boton = document.getElementsByClassName("link")[0];
+
+boton.addEventListener("click", function() {
+  compra();
+});
